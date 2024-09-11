571a630f4288869593dc8e8641a00cb86b7ef272
ACash01 <125855363+ACash01@users.noreply.github.com>
2024-09-11 14:21:35 +0800

    testing ar js feasibility

--- testing/cog-exp/js/app.js
+++ testing/cog-exp/js/app.js
@@ -1,10 +1,11 @@
-//importing three.js
-import * as THREE from 'three'
+import * as THREE from './three.js'
 
-//canvas setup 
+//canvas setup
 const threejsCanvas = document.querySelector('#threejs-canvas')
 let width = threejsCanvas.offsetWidth
 let height = threejsCanvas.offsetHeight
+
+import { ARButton } from 'https://unpkg.com/three@0.168.0/examples/jsm/webxr/ARButton.js'
 
 //camera setup
 const scene = new THREE.Scene()
@@ -13,17 +14,18 @@ camera.position.set(10,10,10)
 camera.lookAt(0,0,0)
 
 //renderer
-const renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({ 
 	antialias: true,
 	alpha: true,
 });
 renderer.setSize( width, height );
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.xr.enabled = true
 threejsCanvas.appendChild(renderer.domElement)
 
 //3d box
 const geometry = new THREE.BoxGeometry(5, 5, 5)
-const material = new THREE.MeshBasicMaterial({color: 0x00ffff})
+const material = new THREE.MeshBasicMaterial({color: 0xffffff})
 const box = new THREE.Mesh(geometry, material)
 scene.add(box)
 
@@ -33,3 +35,6 @@ function update() {
 	renderer.render( scene, camera );
 	window.requestAnimationFrame(update)
 }
+
+const button = ARButton.createButton(renderer)
+document.body.appendChild(button)
